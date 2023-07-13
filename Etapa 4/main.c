0b7ed4fff8b52c42cebcea0b6404e8247b872674
Andrei <andrei.koenich@gmail.com>
2023-07-12 21:29:57 -0300

    Etapa 4 updated

--- Etapa 4/main.c
+++ Etapa 4/main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include "arvore.h"
+
 extern int yyparse(void);
 extern int yylex_destroy(void);
 void *arvore = NULL;
-void exporta (void *arvore){
-  if(arvore == NULL){
-    return;
-  }
-  else{
-    Nodo* nodo = (Nodo*) arvore;
-    printf("%p [label=\"%s\"];\n", nodo, nodo->info->valor_token);
-    //printf("linha_token: %d\n", nodo->info->linha_token);
-    //printf("tipo_token: %d\n", nodo->info->tipo_token);
-    //printf("numero_filhos: %d\n", nodo->numeroFilhos);
-    for (int i = 0; i < nodo->numeroFilhos; i++)
+
+void exporta (void *arvore)
+{
+    if(arvore == NULL)
+        return;
+
+    else
     {
-      printf("%p, %p\n", nodo, nodo->filho[i]);
-      exporta(nodo->filho[i]);
+        Nodo* nodo = (Nodo*) arvore;
+        printf("%p [label=\"%s\"];\n", nodo, nodo->info->valor_token);
+        for (int i = 0; i < nodo->numeroFilhos; i++)
+        {
+            printf("%p, %p\n", nodo, nodo->filho[i]);
+            exporta(nodo->filho[i]);
+        }
     }
-    //printf("%p\n", nodo);
-  }
 }
+
 int main (int argc, char **argv)
 {
-  int ret = yyparse(); 
-  exporta (arvore);
-  yylex_destroy();
-  return ret;
+    int ret = yyparse();
+    exporta (arvore);
+    yylex_destroy();
+    return ret;
 }
