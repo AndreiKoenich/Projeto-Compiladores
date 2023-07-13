a46fa37efdbb035d96fbb15128cdf9b525dba49d
Andrei <andrei.koenich@gmail.com>
2023-07-13 20:30:52 -0300

    Add files via upload

---
+++ Etapa 4/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+extern int yyparse(void);
+extern int yylex_destroy(void);
+void *arvore = NULL;
+Lista_tabelas *lista_tabelas = NULL;
+Tabela *tabela_global = NULL;
+char tipo_atual[QUANTIDADE_CARACTERES_TIPO];
+
+void exporta (void *arvore)
+{
+    if(arvore == NULL)
+        return;
+
+    else
+    {
+        Nodo* nodo = (Nodo*) arvore;
+        printf("%p [label=\"%s\"];\n", nodo, nodo->info->valor_token);
+        for (int i = 0; i < nodo->numeroFilhos; i++)
+        {
+            printf("%p, %p\n", nodo, nodo->filho[i]);
+            exporta(nodo->filho[i]);
+        }
+    }
+}
+
+int main (int argc, char **argv)
+{
+    int ret = yyparse();
+    exporta (arvore);
+    yylex_destroy();
+    return ret;
+}
